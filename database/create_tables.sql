74ecbe976e525f01bfa8cc92af4e67f4f2b8ed55
Cauai Capozzoli <cauaiagro@gmail.com>
2025-04-28 22:35:03 -0300

    commitando

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -1,11 +1,10 @@
--- Criação da tabela
 CREATE TABLE IF NOT EXISTS weather_events (
-    id SERIAL PRIMARY KEY,
-    station_name VARCHAR(50),
-    event_timestamp TIMESTAMP,
-    temperature FLOAT,
-    humidity FLOAT,
-    precipitation FLOAT,
-    wind_speed FLOAT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    id SERIAL PRIMARY KEY,                     -- ID autoincremento único (chave primária)
+    station_name VARCHAR(50),                   -- Nome da estação (texto até 50 caracteres)
+    event_timestamp TIMESTAMP,                  -- Data e hora do evento de clima
+    temperature FLOAT,                          -- Temperatura em graus Celsius
+    humidity FLOAT,                             -- Umidade relativa do ar (em %)
+    precipitation FLOAT,                        -- Precipitação em mm (chuva)
+    wind_speed FLOAT,                           -- Velocidade do vento em km/h ou m/s (dependendo da fonte)
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP -- Momento da inserção no banco, preenchido automaticamente
 );
